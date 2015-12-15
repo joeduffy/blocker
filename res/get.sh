3aa72d7499c6b5e50d8ca03d8f8312c065958f65
Joe Duffy <joeduffy@acm.org>
2015-12-15 12:16:47 -0800

    Adopt SystemD for initialization, not Upstart

--- res/get.sh
+++ res/get.sh
@@ -22,7 +22,7 @@ fi
 
 version=`curl -sSL https://raw.githubusercontent.com/joeduffy/blocker/master/res/LATEST`
 filename="blocker.$version.Linux-x86_64.tar.gz"
-download="https://github.com/joeduffy/blocker/releases/download/v0.1/$filename"
+download="https://github.com/joeduffy/blocker/releases/download/$version/$filename"
 
 echo "Downloading Blocker $version..."
 $sh_c "curl -sSL $download | tar xz"
@@ -33,10 +33,11 @@ $sh_c 'chmod +x /usr/local/bin/blocker'
 $sh_c 'mkdir -p /etc/blocker/.aws'
 $sh_c 'mkdir -p /etc/docker/plugins'
 $sh_c 'echo "unix:///var/run/blocker.sock" > /etc/docker/plugins/blocker.spec'
-$sh_c 'mv blocker.conf /etc/init/blocker.conf'
+$sh_c 'mv blocker.service /etc/systemd/system/blocker.service'
 
 echo "Starting the Blocker service..."
-$sh_c 'service blocker start'
+$sh_c 'systemctl enable /etc/systemd/system/blocker.service'
+$sh_c 'systemctl start blocker.service'
 
 echo "Done."
 
